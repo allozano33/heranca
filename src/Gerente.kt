2ba785afdcff31fc37790c3e4c6c542b4a5784e1
allozano33 <alessandro.lozano@mercadolivre.com>
2022-06-14 10:44:15 -0300

    gerente

--- src/Gerente.kt
+++ src/Gerente.kt
@@ -1,10 +1,13 @@
-class Funcionario(
+class Gerente(
     val nome: String,
     val cpf: String,
     val salario: Double
-
 ) {
     fun bonificacao(): Double {
         return salario * 0.1
+    }
+
+    fun autentica(senha: Int) {
+
     }
 }
--- src/main.kt
+++ src/main.kt
@@ -1,11 +1,13 @@
 fun main() {
     println("Bem vindo ao Bytebank")
 
-val alex = Funcionario(
-    nome = "Alex",
-    cpf = "111.111.111-11",
-    salario = 1000.0
-)
+    val alex = Funcionario(
+        nome = "Alex",
+        cpf = "111.111.111-11",
+        salario = 1000.0,
+        tipo = 2
+
+    )
     println("nome ${alex.nome}")
     println("cpf ${alex.cpf}")
     println("salario ${alex.salario}")
